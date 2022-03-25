e119b66c8938cd8caabfd51581f418117afe58db
DanielNxx <92968031+DanielNxx@users.noreply.github.com>
2022-03-25 12:31:10 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>szkoła zseit DN i MG</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id = "contener">
+
+        <div id="logo"><img ><h1>zespół szkół ekonomicznych i telekomunikacji<h1>
+</div>
+        <div id="navi"style="float:left;">
+            <div class ="menu"><a>nowiny</a></div>
+            <div class ="menu"><a>o szkole</a></div>
+            <div class ="menu"><a>uczniowie</a></div>
+            <div class ="menu"><a>nauczyciele</a></div>
+            <div class ="menu" ><a>dokumenty</a></div>
+        </div>
+        <div id="article"></div>
+        <div id="sub"></div>
+        <div id="footer"></div>
+    </div>
+</body>
+</html>
